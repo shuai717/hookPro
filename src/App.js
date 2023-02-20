de4b6fe9b2e161d08836e76aa1370dfca6ca3e46
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 <1824757520@qq.com>
2023-02-20 22:54:15 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+
+import {useState , useEffect ,useMemo,useCallback} from 'react'
 import './App.css';
 
 function App() {
+  const [num,setNum]=useState(0)
+  const add=()=>{
+    setNum(num+1)
+  }
+  const callback =useCallback(()=>{
+    console.log(num,'222')
+    return num
+  },[num])
+  // 返回一个缓存的函数，添加依赖项数组可以避免函数的无意义计算，降低了子组件的渲染开销。
+  const memo=useMemo((memo,memo2)=>{
+    console.log(memo,memo2,num,'memo,memo2') //实参 为undefined undefined
+    return num
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[])//依赖项 如果不写则除了初始化之后不会运行
+  useEffect((effect1,effect2)=>{
+    console.log(effect1,effect2,num,'num')
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    //return清除副作用函数（如监听、订阅、计时器等）
+  },[num])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{num}</div>
+      mome{memo}
+      callback{callback()}
+      <button onClick={add}>click</button>
     </div>
   );
 }
